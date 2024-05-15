b92001284185b1c8450ec454d125523027d07e44
kapiado <apiado.katrina@gmail.com>
2024-05-15 12:29:13 -0700

    Added 4 new pages

---
+++ pages/2_About_The_Data.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import streamlit.components.v1 as components
+
+
+# st.set_page_config(
+#     page_title="Background",
+#     page_icon="👋",
+# )
+#st.markdown("# Overview")
+st.set_page_config(page_title="About the Data")
+st.write("# About the Data")
---
+++ pages/3_Data_Cleaning.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import streamlit.components.v1 as components
+
+
+# st.set_page_config(
+#     page_title="Background",
+#     page_icon="👋",
+# )
+#st.markdown("# Overview")
+st.set_page_config(page_title="Data Cleaning")
+st.write("# Data Cleaning")
