719e56e013cdf2bff7ccf28ca551fb25ffb01b77
kapiado <apiado.katrina@gmail.com>
2024-05-15 22:27:38 -0700

    Updating background page

--- pages/1_Background.py
+++ pages/1_Background.py
@@ -2,10 +2,11 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 
-# st.set_page_config(
-#     page_title="Background",
-#     page_icon="👋",
-# )
-#st.markdown("# Overview")
+st.set_page_config(
+    page_title="Background",
+    page_icon="👋",
+)
+st.markdown("# Background")
 st.set_page_config(page_title="Background")
 st.write("# Background")
+
