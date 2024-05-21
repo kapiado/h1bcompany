6854de937fb98d3533193f9286b009b141bc5928
kapiado <apiado.katrina@gmail.com>
2024-05-21 11:06:57 -0700

    showing df

--- pages/5_H-1B_Visa_Company_Recommendation_Form.py
+++ pages/5_H-1B_Visa_Company_Recommendation_Form.py
@@ -2,6 +2,7 @@
 # Import libraries
 import pandas as pd             # Pandas
 import streamlit as st          # Streamlit
+import numpy as np              # NumPy
 # import matplotlib.pyplot as plt # Matplotlib
 # import seaborn as sns           # Seaborn
 
@@ -24,8 +25,68 @@ st.title('H-1B Visa Company Recommendation Form')
 st.write("This app recommends companies that are likely to sponsor an H-1B visa based on your user input. Use the form below to get started!")
          #uses *x* inputs to determine companies that would be a good fit for you to apply for. Use the form below to get started!")
 
-# df2 = pd.read_csv("pages/11_30_23_Pred_Data_Final1.csv")
+# df_original = pd.read_csv("merged_data_5_16_24.csv")
 
+# # create copy of compete dataframe to be manipulated
+# df_cleaned = df_original.copy()
+# df_cleaned.info()
+
+# # Select only the relevant columns
+# columns_list = [
+#     'EMPLOYER_NAME_CLEAN',
+#     'EMPLOYER_NAME',
+#     'SOC_TITLE',
+#     'WORKSITE_STATE',
+#     'PREVAILING_WAGE_ANNUAL',
+#     'SECTOR_CODE',
+#     'EMPLOYEE_COUNT_CATEGORY',
+#     'COMPANY_AGE',
+#     'COMPANY_LINK',
+#     'SPONSORED_2012.0',
+#     'SPONSORED_2013.0',
+#     'SPONSORED_2014.0',
+#     'SPONSORED_2015.0',
+#     'SPONSORED_2016.0',
+#     'SPONSORED_2017.0',
+#     'SPONSORED_2018.0',
+#     'SPONSORED_2019.0',
+#     'SPONSORED_2020.0',
+#     'SPONSORED_2021.0',
+#     'SPONSORED_2022.0',
+#     'SPONSORED_2023.0',
+#     'SPONSORED_2024.0'
+# ]
+# df_cleaned = df_cleaned.loc[:, columns_list]
+
+# # sponsorship year weights
+# sponsorship_weights = {
+#     'SPONSORED_2012.0': 0.0294,
+#     'SPONSORED_2013.0': 0.0294,
+#     'SPONSORED_2014.0': 0.0294,
+#     'SPONSORED_2015.0': 0.0294,
+#     'SPONSORED_2016.0': 0.0588,
+#     'SPONSORED_2017.0': 0.0588,
+#     'SPONSORED_2018.0': 0.0588,
+#     'SPONSORED_2019.0': 0.0882,
+#     'SPONSORED_2020.0': 0.0882,
+#     'SPONSORED_2021.0': 0.0882,
+#     'SPONSORED_2022.0': 0.1471,
+#     'SPONSORED_2023.0': 0.1471,
+#     'SPONSORED_2024.0': 0.1471
+# }
+
+# # combine the number of sponsored visas per year into one columns
+# df_cleaned['SPONSORED'] = 0.0
+
+# for col, weight in sponsorship_weights.items():
+#     df_cleaned['SPONSORED'] += df_cleaned[col] * weight
+    
+# df_cleaned['SPONSORED'] = df_cleaned['SPONSORED'].round()
+
+# # drop all the sponsored counts columns besides the combined value
+# columns_to_drop = list(sponsorship_weights.keys())
+# columns_to_drop.remove('SPONSORED_2023.0')
+# df_cleaned.drop(columns=columns_to_drop, inplace=True)
 # df2.drop(['WAITING_TIMERANGE'], axis=1, inplace=True)
 
 
@@ -40,8 +101,20 @@ st.write("This app recommends companies that are likely to sponsor an H-1B visa 
 # EMPLOYEE COUNT CATEGORY
 # COMPANY AGE CATEGORY
 
+# # Connect to the Google Sheet of SOC TITLEs
+# sheet_id = "1oLjpm4KLNj-tUN_Pnbrk_ihU7bNylJwG"
+# sheet_name = "Final"
+# url = f"<https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}>"
+# df = pd.read_csv(url, dtype=str).fillna("")
+
+
+
+# # Show the dataframe (we'll delete this later)
+# st.write(df)
+
     # Some code
 with st.form(key='my_form'):
+    # all codes
     codeOptions = ['11 - Agriculture, Forestry, Fishing and Hunting', 
                    '21 - Mining, Quarrying, and Oil and Gas Extraction', 
                    '22 - Utilities', 
@@ -67,37 +140,105 @@ with st.form(key='my_form'):
                    '81 - Other Services (except Public Administration)',
                    '92 - Public Administration']
     
-
+    # codes only in dataset
+    codeOptions2 = ['11 - Agriculture, Forestry, Fishing and Hunting', 
+                   '22 - Utilities', 
+                   '31 - Manufacturing (Food, Beverage, Tobacco, Apparel, Leather, Textiles)', 
+                   '32 - Manufacturing (Paper, Printing, Petroleum, Coal, Chemicals, Plastics, Rubber, Nonmetallic)', 
+                   '33 - Manufacturing (Primary Metals, Fabricated Metal, Machinery, Computer and Electronic Products, Electrical Equipment and Appliances, Transportations Equipment, Furniture, Miscellaneous Manufacturing)', 
+                   '42 - Wholesale Trade',
+                   '44 - Retail Trade (Automotive Sales and Services, Home Furnishing and Improvement, Food and Beverage, Health and Personal Care, Clothing and Accessories, Gasoline Stations)',
+                   '45 - Retail Trade (Sporting Goods, Hobbies, Books, Department Stores, General Merchandise Stores, Florists, Office Supplies, Pet Supplies, Art Dealers, Various Specialty Stores)', 
+                   '48 - Transportation and Warehousing (Air, Rail, Water, Truck, Transit, Pipeline, Scenic and Sightseeing Services, Transportation Support Activities)', 
+                   '51 - Information',
+                   '52 - Finance and Insurance',
+                   '53 - Real Estate and Rental and Leasing',
+                   '54 - Professional, Scientific, and Technical Services',
+                   '55 - Management of Companies and Enterprises',
+                   '56 - Administrative and Support and Waste Management and Remediation Services',
+                   '61 - Educational Services',
+                   '62 - Health Care and Social Assistance',
+                   '71 - Arts, Entertainment, and Recreation',
+                   '72 - Accommodation and Food Services',
+                   '81 - Other Services (except Public Administration)',
+                   ]
+    
+    MAX_SELECTIONS = 3
 # To get the selected value from the select box
-    codeInfo = st.selectbox('NAICS Code', codeOptions, help="Select most appropriate Industry Code as found here https://www.census.gov/naics/?58967?yearbck=2022")
+    # codeInfo = st.selectbox('Select industry (up to 3)', codeOptions2, help="Select most appropriate Industry Code as found here https://www.census.gov/naics/?58967?yearbck=2022")
+    codeInfo = st.multiselect('Select industry (up to 3)', codeOptions2, help="Select most appropriate Industry Code as found here https://www.census.gov/naics/?58967?yearbck=2022")
 
+    if len(selected_states) > MAX_SELECTIONS:
+    st.error(f"Please select up to {MAX_SELECTIONS} states only.")
+    selected_states = selected_states[:MAX_SELECTIONS]
+    # have them choose the weights
+    # 1 - 5
 
-    stateInfo = st.selectbox('U.S. Work State',
-                                                  [
-    'ALABAMA', 'ALASKA', 'ARIZONA', 'ARKANSAS', 'CALIFORNIA', 'COLORADO', 'CONNECTICUT', 'DELAWARE',
-    'DISTRICT OF COLUMBIA', 'FLORIDA', 'GEORGIA', 'GUAM', 'HAWAII', 'IDAHO', 'ILLINOIS', 'INDIANA',
-    'IOWA', 'KANSAS', 'KENTUCKY', 'LOUISIANA', 'MAINE', 'MARSHALL ISLANDS', 'MARYLAND', 'MASSACHUSETTS',
-    'MICHIGAN', 'MINNESOTA', 'MISSISSIPPI', 'MISSOURI', 'MONTANA', 'NEBRASKA', 'NEVADA', 'NEW HAMPSHIRE',
-    'NEW JERSEY', 'NEW MEXICO', 'NEW YORK', 'NORTH CAROLINA', 'NORTH DAKOTA', 'NORTHERN MARIANA ISLANDS',
-    'OHIO', 'OKLAHOMA', 'OREGON', 'PENNSYLVANIA', 'PUERTO RICO', 'RHODE ISLAND', 'SOUTH CAROLINA',
-    'SOUTH DAKOTA', 'TENNESSEE', 'TEXAS', 'UTAH', 'VERMONT', 'VIRGIN ISLANDS', 'VIRGINIA', 'WASHINGTON',
-    'WEST VIRGINIA', 'WISCONSIN', 'WYOMING'
-], 
+    # take the number assignemt, 2/5
+    # add up all numbers and then divide number they chose over the total amount = 1
+    # input "X" = Xi/SUM(all X) for all Xi
+    # each value over the sum of all the values
+    # x = 3
+    # y = 5
+    # z = 2
+    # sum = 10
+    # xnorm = 3/10
+    # ynorm = 5/10
+    # znorm = 2/10
+
+    # ranking states
+    # make a list
+
+    # key to dictionary is column and value is a list
+    # needs to be 
+
+    state_abbreviations = [
+    "AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", 
+    "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", 
+    "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", 
+    "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", 
+    "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"
+]
+
+    # ranking, enabling them to choose multiple options
+    # Need to abbreviate these states
+    stateInfo = st.selectbox('U.S. Work State', state_abbreviations, 
                              help = "Select the U.S. state of primary worksite")
 
+    employeenum_categories = ['<50', '51-200', '201-500', '501-1000', 
+                              '1001-5000','5001-10000', '10000+']
     #employeenumInfo = st.number_input('Number of Employees at Company', min_value = 0)
-    employeenumInfo = st.selectbox('Number of Employees at Company', ['<50', '51-200', '201-500',
-                                                                      '501-1000', '1001-5000',
-                                                                      '5001-10000', '10000+'])
+    employeenumInfo = st.selectbox('Number of Employees at Company', employeenum_categories)
 
-    companyageInfo = st.selectbox('Age Category',
-                                  ['Newly Established (0-2 years)','Early Stage (2-5 years)',
+    companyage_categories = ['Newly Established (0-2 years)','Early Stage (2-5 years)',
                                    'Growth Stage (5-10 years)','Mature Stage (10-20 years)',
-                                   'Established Stage (20-50 years)', 'Legacy Stage (50+ years)'])
+                                   'Established Stage (20-50 years)', 'Legacy Stage (50+ years)']
+
+    companyageInfo = st.select_slider('Age Category', companyage_categories)
 
     submit = st.form_submit_button('Submit',args=(1,
                     [codeInfo, stateInfo, employeenumInfo, companyageInfo]))
 
+
+# user preferences
+user_preferences = {
+    'SOC_TITLE': ['COMPUTER SYSTEMS ANALYST', 'COMPUTER PROGRAMMER', 'ECONOMISTS'],
+    'WORKSITE_STATE': ['CA', 'TX', 'NY'],
+    'SECTOR_CODE': [54, 52, 33],
+    'EMPLOYEE_COUNT_CATEGORY': ['10000+', '501-1000', '51-200'],
+    'COMPANY_AGE_CATEGORY': ['Mature Stage (10-20 years)', 'Growth Stage (5-10 years)', 'Established Stage (20-50 years)'],
+} 
+
+# column weights
+weights = {
+    'SPONSORED': .3,
+    'SOC_TITLE': .2,
+    'WORKSITE_STATE': .2,
+    'PREVAILING_WAGE_ANNUAL': .1,
+    'SECTOR_CODE': .1,
+    'EMPLOYEE_COUNT_CATEGORY': .05,
+    'COMPANY_AGE_CATEGORY': .05,
+}
 # df3 = df2.copy()
 # df3.loc[len(df3)] = [codeInfo, wagelevelInfo, wageamountInfo, stateInfo, countryInfo, employeenumInfo,  admiclassInfo,  jobeducationInfo, expInfo, expmonthsInfo, layoffInfo, educationInfo]
 # # Create dummies for encode_df
