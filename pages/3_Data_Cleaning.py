7d50ef4c9da910e07432f3504393c7b6f3eed298
kapiado <apiado.katrina@gmail.com>
2024-05-23 02:51:43 -0700

    Adding flowchart image to Data Cleaning page, deleting Math Model page, renumbering pages

--- pages/3_Data_Cleaning.py
+++ pages/3_Data_Cleaning.py
@@ -1,5 +1,9 @@
 import streamlit as st
 import streamlit.components.v1 as components
+from PIL import Image
+
+# Adding path to Images folder
+path = "Images/"
 
 
 # st.set_page_config(
@@ -9,3 +13,13 @@ import streamlit.components.v1 as components
 #st.markdown("# Overview")
 st.set_page_config(page_title="Data Cleaning")
 st.write("# Data Cleaning")
+
+# Centered layout
+st.write("")
+col1, col2, col3 = st.columns([1, 2, 1])
+
+# Display the image in the middle column
+# Adding image with caption
+with col2:
+    image = Image.open(path+'h1b_data_flowchart.png')
+    st.image(image)
