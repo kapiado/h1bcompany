f190c5d670b965f6e88ee3cdad1d63561eae635a
kapiado <apiado.katrina@gmail.com>
2024-06-12 19:55:08 -0700

    Adding territory to caption to select U.S. work state

--- pages/4_H-1B_Visa_Company_Recommendation_Form.py
+++ pages/4_H-1B_Visa_Company_Recommendation_Form.py
@@ -653,7 +653,7 @@ with st.form(key='my_form'):
 
     state_full_names = sorted(df_cleaned["FULL_WORKSITE_STATE"].unique().tolist())
 
-    stateInfo = st.multiselect('Select U.S. Work State(s)', state_full_names, help="Select the state where you would like to work.")
+    stateInfo = st.multiselect('Select U.S. Work State/Territory(s)', state_full_names, help="Select the state/territory where you would like to work.")
 
     employeenum_categories = ['<50', '51-200', '201-500', '501-1000', '1001-5000', '5001-10000', '10000+']
     employeenumInfo = st.multiselect('Select Company Size(s)', employeenum_categories)
