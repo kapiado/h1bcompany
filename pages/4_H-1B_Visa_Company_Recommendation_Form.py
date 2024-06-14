58f4d115687f7c61ac1f6f6ca5266818097674de
kapiado <apiado.katrina@gmail.com>
2024-06-13 18:11:08 -0700

    fixing output from form

--- pages/4_H-1B_Visa_Company_Recommendation_Form.py
+++ pages/4_H-1B_Visa_Company_Recommendation_Form.py
@@ -758,7 +758,7 @@ if submit_button:
 
             # Display only unique outputs
             result_df.drop_duplicates(subset=['EMPLOYER_NAME_CLEAN'], keep='first', inplace=True)
-            result_df = result_df[['EMPLOYER_NAME', 'SOC_TITLE', 'FULL_WORKSITE_STATE', 'PREVAILING_WAGE_ANNUAL', 
+            result_df = result_df[['EMPLOYER_NAME', 'SOC_TITLE', 'WORKSITE_STATE', 'PREVAILING_WAGE_ANNUAL', 
                                 'EMPLOYEE_COUNT_CATEGORY', 'COMPANY_AGE_CATEGORY', 'COMPANY_LINK', 'SPONSORED', 
                                 'JOB_COUNT', 'OTHER_WORKSITE_STATE', 'OTHER_SOC_TITLES']]
 
